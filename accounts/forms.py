37a86ff2974eabb0baaa534e1326af23c01ec908
Hassan <mahassantech@gmail.com>
2025-09-24 23:43:56 +0600

    sign up logic add

--- accounts/forms.py
+++ accounts/forms.py
@@ -2,8 +2,8 @@ from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
 
-
 class SignUpForm(UserCreationForm):
+    email = forms.EmailField(required=True, label="Email")
     class Meta: 
         model=User
-        fields=['username','email',]
+        fields=['username','email']
--- accounts/views.py
+++ accounts/views.py
@@ -1,10 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .forms import SignUpForm
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 def SignUp(request):
-    form=SignUpForm()
+    if request.method=="POST":
+        form=SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form=SignUpForm()
     return render(request,'accounts/signup.html',{'form':form})
 
 def SignIn(request):
-    return render(request,'accounts/signin.html')
+    form=AuthenticationForm()
+    return render(request,'accounts/signin.html',{'form':form})
