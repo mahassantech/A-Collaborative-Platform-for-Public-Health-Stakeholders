abc1e94c48efc05c4969f27f1319e2c4a775524d
Hassan <mahassantech@gmail.com>
2025-09-25 01:34:33 +0600

    sign in logic added

--- accounts/views.py
+++ accounts/views.py
@@ -2,24 +2,18 @@ from django.shortcuts import render,redirect
 from .forms import SignUpForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.views.generic import CreateView
+from django.contrib.auth.views import LoginView
+from django.contrib.auth import logout
 from django.urls import reverse_lazy
 
-# Create your views here.
-# def SignUp(request):
-#     if request.method=="POST":
-#         form=SignUpForm(request.POST)
-#         if form.is_valid():
-#             form.save()
-#             return redirect('login')
-#     else:
-#         form=SignUpForm()
-#     return render(request,'accounts/signup.html',{'form':form})
 
 class SignUpView(CreateView):
     template_name='accounts/signup.html'
     form_class=SignUpForm
     success_url=reverse_lazy('login')
 
-def SignIn(request):
-    form=AuthenticationForm()
-    return render(request,'accounts/signin.html',{'form':form})
+class SignInView(LoginView):
+    template_name='accounts/signin.html'
+    
+    def get_success_url(self):
+        return reverse_lazy('profile')
