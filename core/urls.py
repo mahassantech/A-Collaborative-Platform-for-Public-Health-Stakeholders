0a471f1cf443ef214e3607348c1a3f1a2d210f67
Hassan <mahassantech@gmail.com>
2025-09-28 23:42:30 +0600

    blog function add

--- core/urls.py
+++ core/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    # path('',views.Home),
+    path('',views.BlogPage,name='blogpage'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
 # Create your views here.
+def BlogPage(request):
+    return render(request, "core/blog.html")
