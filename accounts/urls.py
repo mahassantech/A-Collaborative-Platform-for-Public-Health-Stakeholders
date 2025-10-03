70e07dabe10df0cbf7c23d0a2df548914c2cc942
Hassan <mahassantech@gmail.com>
2025-10-03 23:36:33 +0600

    edit

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ from .views import SignUpView,SignInView
 urlpatterns = [
     path('signup/',SignUpView.as_view(),name='signup'),
     path('login/',SignInView.as_view(),name='login'),
+    path('profile/',SignInView.as_view(),name='profile'),
 ]
