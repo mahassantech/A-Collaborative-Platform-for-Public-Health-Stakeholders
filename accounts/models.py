d969e9b4543af82a32a3b073e64a5a4dcf6cd110
Hassan <mahassantech@gmail.com>
2025-10-07 22:39:48 +0600

    accounts model add

--- accounts/models.py
+++ accounts/models.py
@@ -1,17 +1,15 @@
-from django.db import models
+# accounts/models.py
 from django.contrib.auth.models import AbstractUser
-from .constants import ROLE_CHOICES
+from django.db import models
 
-# Create your models here.
-class User(AbstractUser):
-    role = models.CharField(max_length=30, choices=ROLE_CHOICES)
-    token = models.CharField(max_length=100, unique=True)
+class CustomUser(AbstractUser):
+    ROLE_CHOICES = [
+        ('member', 'Member'),
+        ('provider', 'Data Provider'),
+        ('analyst', 'Analyst'),
+        ('admin', 'Admin'),
+    ]
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES, default='member')
 
     def __str__(self):
         return f"{self.username} ({self.role})"
-    
-# class Membership(models.Model):
-#     user_id=models.CharField(unique=True)
-#     Membership_id=models.CharField(unique=True)
-#     role=models.CharField(max_length=30,choices=ROLE_CHOICES)
-#     token_number=token = models.CharField(max_length=100, unique=True)
